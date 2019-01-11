6d1b5e831f10ef97d886767c4547ba3cd4dfc782
久原拓也 <takuya@kuharatakuyanoMacBook-Air.local>
2019-01-11 09:22:50 +0900

    Fix format

--- ucc.c
+++ ucc.c
@@ -1,15 +1,14 @@
+#include "ucc.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "ucc.h"
-
 
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "引数の個数が正しくありません\n");
     return 1;
   }
-  if ( strcmp(argv[1], "-test") == 0 ) {
+  if (strcmp(argv[1], "-test") == 0) {
     runtest();
     return 0;
   }
